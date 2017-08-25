ad4d66483d603d0bf3073b570792c8280fce0a47
joygeorgec <joygeorge.c@gmail.com>
2017-08-25 12:46:32 +0530

    Changes to homepage buttons
    Changes to homepage buttons

--- resources/js/script.js
+++ resources/js/script.js
@@ -15,13 +15,13 @@ $(document).ready(function () {
 
     
     /*Scrolling buttons*/
-    $('.js--scroll-to-plans').click(function(){
-        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);    
+    $('.js--scroll-to-services').click(function(){
+        $('html, body').animate({scrollTop: $('.js--section-services').offset().top}, 1000);    
     });
     
 
-    $('.js--scroll-to-start').click(function(){
-        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 500);    
+    $('.js--scroll-to-feedback').click(function(){
+        $('html, body').animate({scrollTop: $('.js--section-feedback').offset().top}, 500);    
     });
     
 
